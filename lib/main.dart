0ed1cb836733344d392190b56d3eca52d9372a41
Dharm2804 <kasundradharm2845@gmail.com>
2025-04-30 15:45:34 +0530

    re correct a data base for initialize

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
     WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  Hive.registerAdapter(LoyaltyCardAdapter());
-  await Hive.openBox<LoyaltyCard>('loyaltyCards');
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
